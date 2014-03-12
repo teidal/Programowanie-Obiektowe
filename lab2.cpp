f60cd07e3eb6d3a41be9aacc0f044510bf9c610d
teidal <209919@student.pwr.edu.pl>
2014-03-12 20:30:03 +0100

    pierwszy raz

---
+++ lab2.cpp
@@ -0,0 +1,55 @@
+// lab2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+
+struct _location{
+	float x,y;
+};
+
+class Circle{
+	_location location;
+	float radius;
+public:
+	void set_location(float x,float y){location.x=x; location.y=y;}
+	void set_radius(float r){radius = r;}
+	_location get_location(){return location;}
+	float get_radius(){return radius;}
+
+	float circumference()
+{
+	return 2 * M_PI * radius;
+};
+	float area()
+{
+	return M_PI * pow(radius,2);
+};
+};
+
+int main()
+{
+	Circle circle;
+circle.set_location(4., 2.);
+circle.set_radius(15.);
+
+printf("Circle location is %.1fx%.1f.\n", 
+    circle.get_location().x, circle.get_location().y);
+printf("Circle radius is %.1f.\n", 
+    circle.get_radius());
+printf("Circle circumference is %.2f.\n", 
+    circle.circumference());
+printf("Circle area is %.2f.\n", 
+    circle.area());
+
+system("pause");
+}
+
+
+
+
+
